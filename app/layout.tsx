7ef46cd852f1b709f6d08ae171461ef4ecf51c41
tajuddinjntu-star <tajuddinjntu@gmail.com>
2026-04-28 03:53:58 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 import type { ReactNode } from "react";
-
 export const metadata = {
   title: "Sirajuddin Software Technologies",
   description: "WeldWise Assistant Software Platform",
 };
-
 export default function RootLayout({
   children,
 }: {
